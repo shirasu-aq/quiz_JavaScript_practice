0aed07c5ddcde7f08e7c0c31c91add5a29f5dba7
Risako Shirasu <shirasu@S678.local>
2021-05-31 10:50:59 +0900

    async await ver

--- main.js
+++ main.js
@@ -2,32 +2,31 @@
 
 // fetchリクエストの設定
 const API_URL = "https://opentdb.com/api.php?amount=10&type=multiple";
-fetch(API_URL) 
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    const quizData = data.results;
-    const quizListContainer = document.getElementById('quiz-list');
 
-    quizData.forEach((quiz, index) => {
-      const numberTitleItem = document.createElement('li');
-      numberTitleItem.textContent = `${index + 1}件目のクイズデータ`;
-      quizListContainer.appendChild(numberTitleItem);
+const fetchFunction = async () => {
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  const quizData = data.results;
+
+  const quizListContainer = document.getElementById('quiz-list');
+
+  quizData.forEach((quiz, index) => {
+    const numberTitleItem = document.createElement('li');
+    numberTitleItem.textContent = `${index + 1}件目のクイズデータ`;
+    quizListContainer.appendChild(numberTitleItem);
+
+    const buildQuizList = (quiz) => {
+      const quizContainer = document.createElement('ul');
+      for(const prop in quiz) {
+        const item = document.createElement('li');
+        item.innerHTML = `<storong>${prop}</storong>: ${quiz[prop]}`;
+        quizContainer.appendChild(item);
+      }
+      return quizContainer;
+    }
 
     const quizDataList = buildQuizList(quiz);
-    numberTitleItem.appendChild(quizDataList);  
-    })
+    numberTitleItem.appendChild(quizDataList);
   })
+}
 
-  const buildQuizList = (quiz) => {
-    const quizContainer = document.createElement('ul');
-    for(const prop in quiz) {
-      const item = document.createElement('li');
-      item.innerHTML = `<storong>${prop}</storong>: ${quiz[prop]}`;
-      quizContainer.appendChild(item);
-    }
-    return quizContainer;
-  };
-
-  

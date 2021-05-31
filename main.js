32afda0b5f551d1adf78637badc4dfa6114be8b7
Risako Shirasu <shirasu@S678.local>
2021-05-31 11:43:37 +0900

    setquizに引数を渡す

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-const API_URL = "https://opentdb.com/api.php?amount=10&type=multiple";
+const API_URL = 'https://opentdb.com/api.php?amount=10&type=multiple';
 
 class Quiz {
-    constructor(quizData) {
-      // クイズの情報
-      this._quizees = quizData.results;
-    }
+　constructor(quizData) {
+　　this._quizzes = quizData.results;
+　}
 
-    getQuizCategory(index) {
-      return this._quizees[index - 1].category;
-    }
+　getQuizCategory(index) {
+　　return this._quizzes[index - 1].category;
+　}
 
-    getQuizDifficulty(index) {
-      return this._quizees[index - 1].difficulty;
-    }
+　getQuizDifficulty(index) {
+　　return this._quizzes[index - 1].difficulty;
+　}
 
-    getQuizQuestion(index) {
-      return this._quizees[index - 1].question;
-    }
+　getQuizQuestion(index) {
+　　return this._quizzes[index - 1].question;
+　}
 };
 
 const titleElement = document.getElementById('title');
@@ -32,38 +31,28 @@ startButton.addEventListener('click', () => {
 });
 
 const fetchQuizData = async (index) => {
-    titleElement.textContent = '取得中';
-    questionElement.textContent = '少々お待ちください';
+　titleElement.textContent = '取得中';
+　questionElement.textContent = '少々お待ち下さい';
 
-    const response = await fetch(API_URL);
-    const quizData = await response.json();
-    // fetchで受け取ったクイズのデータをインスタンスの引数に設置する
-    const quizInstance = new Quiz(quizData);
+　const response = await fetch(API_URL);
+　const quizData = await response.json();
+　const quizInstance = new Quiz(quizData);
 
-    setNextQuiz(quizInstance, index);
+　setNextQuiz(quizInstance, index);
 };
 
 const makeQuiz = (quizInstance, index) => {
+　titleElement.innerHTML = `問題 ${index}`;
+　genreElement.innerHTML = `【ジャンル】 ${quizInstance.getQuizCategory(index)}`;
+　difficultyElement.innerHTML = `【難易度】 ${quizInstance.getQuizDifficulty(index)}`;
+　questionElement.innerHTML = `【クイズ】${quizInstance.getQuizQuestion(index)})`;
 
-  titleElement.innerHTML = `問題${index}`;
-  // クラスで定義したメソッドを利用
-  genreElement.innerHTML = `【ジャンル】 ${quizInstance.getQuizCategory(index)}`;
-  difficultyElement.innerHTML = `【難易度】 ${quizInstance.getQuizDifficulty(index)}`;
-  questionElement.innerHTML = `【クイズ】 ${quizInstance.getQuizQuestion(index)}`;
-
-  const buttonElement = document.createElement('button');
-  buttonElement.innnerHTML = '次へ';
-  answerContainer.appendChild(buttonElement);
-
-  buttonElement.addEventListener('click', () => {
-    index++;
-    answersContainer.removeChild(answersContainer.firstChild);
-    // 引数として渡ってきているquizinstanceを用いて、ジャンルや難易度の値を取得
-    setNextQuiz(quizInstance, index);
-  });
-
+　const buttonElement = document.createElement('button');
+　buttonElement.innerHTML = '次へ';
+　answersContainer.appendChild(buttonElement);
+　buttonElement.addEventListener('click', () => {
+　　index++;
+　　answersContainer.removeChild(answersContainer.firstChild);
+　　setNextQuiz(quizInstance, index);
+　});
 };
-
-
-
-

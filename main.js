cf487dfc6a2d275d532a45222c20a210bfda109d
Risako Shirasu <shirasu@S678.local>
2021-05-31 11:40:59 +0900

    一時保存

--- main.js
+++ main.js
@@ -17,8 +17,19 @@ class Quiz {
     getQuizQuestion(index) {
       return this._quizees[index - 1].question;
     }
+};
 
-}
+const titleElement = document.getElementById('title');
+const questionElement = document.getElementById('question');
+const answersContainer = document.getElementById('answers');
+const startButton = document.getElementById('start-button');
+const genreElement = document.getElementById('genre');
+const difficultyElement = document.getElementById('difficulty');
+
+startButton.addEventListener('click', () => {
+　startButton.hidden = true;
+　　fetchQuizData(1);
+});
 
 const fetchQuizData = async (index) => {
     titleElement.textContent = '取得中';
@@ -32,7 +43,27 @@ const fetchQuizData = async (index) => {
     setNextQuiz(quizInstance, index);
 };
 
+const makeQuiz = (quizInstance, index) => {
+
+  titleElement.innerHTML = `問題${index}`;
+  // クラスで定義したメソッドを利用
+  genreElement.innerHTML = `【ジャンル】 ${quizInstance.getQuizCategory(index)}`;
+  difficultyElement.innerHTML = `【難易度】 ${quizInstance.getQuizDifficulty(index)}`;
+  questionElement.innerHTML = `【クイズ】 ${quizInstance.getQuizQuestion(index)}`;
+
+  const buttonElement = document.createElement('button');
+  buttonElement.innnerHTML = '次へ';
+  answerContainer.appendChild(buttonElement);
+
+  buttonElement.addEventListener('click', () => {
+    index++;
+    answersContainer.removeChild(answersContainer.firstChild);
+    // 引数として渡ってきているquizinstanceを用いて、ジャンルや難易度の値を取得
+    setNextQuiz(quizInstance, index);
+  });
+
+};
 
 
 
-// }
+

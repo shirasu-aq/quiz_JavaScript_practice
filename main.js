4c17d16d1c0e2909cb55d11e6de40ea5a4d627f0
Risako Shirasu <shirasu@S678.local>
2021-05-31 10:37:02 +0900

    promise ver

--- main.js
+++ main.js
@@ -7,5 +7,27 @@ fetch(API_URL)
     return res.json();
   })
   .then((data) => {
-    console.log(data.results);
+    const quizData = data.results;
+    const quizListContainer = document.getElementById('quiz-list');
+
+    quizData.forEach((quiz, index) => {
+      const numberTitleItem = document.createElement('li');
+      numberTitleItem.textContent = `${index + 1}件目のクイズデータ`;
+      quizListContainer.appendChild(numberTitleItem);
+
+    const quizDataList = buildQuizList(quiz);
+    numberTitleItem.appendChild(quizDataList);  
+    })
   })
+
+  const buildQuizList = (quiz) => {
+    const quizContainer = document.createElement('ul');
+    for(const prop in quiz) {
+      const item = document.createElement('li');
+      item.innerHTML = `<storong>${prop}</storong>: ${quiz[prop]}`;
+      quizContainer.appendChild(item);
+    }
+    return quizContainer;
+  };
+
+  

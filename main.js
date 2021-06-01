bf0a14c889cbbcaa90c7e920a1835481b4180d31
Risako Shirasu <shirasu@S678.local>
2021-06-01 11:36:58 +0900

    10問をランダムに表示

--- main.js
+++ main.js
@@ -10,6 +10,7 @@
   const questionElement = document.getElementById("question");
   const genreElement = document.getElementById("genre");
   const difficultyElement = document.getElementById("difficulty");
+  const answersContainer = document.getElementById("answers");
 
   // fetchAPIを用いてデータを取り出す
   const API_URL = "https://opentdb.com/api.php?amount=10&type=multiple";
@@ -20,35 +21,41 @@
     const response = await fetch(API_URL);
     const quizData = await response.json();
     const arrayquizData = quizData.results;
-    // const quizInstance = new Quiz(quizData);
-    // console.log(quizInstance);
-
-    // setNextQuiz(quizInstance, index);
+    let correctAnswersNum = 0;
 
     const showQuestions = () => {
+      // 最初の子要素以外を消す
+      while (answersContainer.firstChild) {
+        answersContainer.removeChild(answersContainer.firstChild);
+      }
+
       const eachQuiz = arrayquizData[index];
       titleElement.innerHTML = `【問題】${index}`;
       genreElement.innerHTML = `【カテゴリー】${eachQuiz.category}`;
       difficultyElement.innerHTML = `【難易度】 ${eachQuiz.difficulty}`;
       questionElement.innerHTML = `【問題】${eachQuiz.question}`;
 
-      // 解答選択肢を定義
+      // 解答選択肢を定義（スプレット構文）
       const answers = [eachQuiz.correct_answer, ...eachQuiz.incorrect_answers];
 
       // 答えの表示
       answers.forEach((answer) => {
         const answerElement = document.createElement("li");
-        const answersContainer = document.getElementById("answers");
         answersContainer.appendChild(answerElement);
 
         const buttonElement = document.createElement("button");
         buttonElement.innerHTML = answer;
         answerElement.appendChild(buttonElement);
 
+        // 回答した際のクリックイベント
         buttonElement.addEventListener("click", () => {
-          quizInstance.countCorrectAnswersNum(index, answer);
+          countCorrectAnsewrsNum = () => {
+            if (answer === eachQuiz.correct_answer) {
+              return correctAnswersNum++;
+            }
+          };
           index++;
-          setNextQuiz(quizInstance, index);
+          setNextQuiz();
         });
         // return shuffleArray(answers);
       });
@@ -62,7 +69,34 @@
         return shuffleArray(answers);
       };
     };
-    showQuestions();
+
+    // 10問全てのクイズ解答が終わった後の画面（正答数表示画面)
+    const finishQuiz = () => {
+      titleElement.textContent = `あなたの正答数は${CorrectAnswersNum()}です`;
+      genreElement.textContent = "";
+      difficultyElement.textContent = "";
+      questionElement.textContent = "再チャレンジしたい場合は下をクリック";
+
+      const restartButton = document.createElement("button");
+      restartButton.textContent = "ホームに戻る";
+      answersContainer.appendChild(restartButton);
+      restartButton.addEventListener("click", () => {
+        location.reload();
+      });
+    };
+
+    // 回答数によって処理を切り分ける
+    const setNextQuiz = () => {
+      while (answersContainer.firstChild) {
+        answersContainer.removeChild(answersContainer.firstChild);
+      }
+      if (index <= 10) {
+        showQuestions();
+      } else {
+        finishQuiz();
+      }
+    };
+    setNextQuiz();
   };
 
   //   // クイズの問題数によって処理を切り分けていく

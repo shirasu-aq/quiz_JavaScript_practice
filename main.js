c5d73fa2284df8ec67d9950ee5bdae7eaac31b0b
Risako Shirasu <shirasu@S734.local>
2021-06-02 11:26:26 +0900

    並べ替え

--- main.js
+++ main.js
@@ -71,21 +71,6 @@
       };
     };
 
-    // 10問全てのクイズ解答が終わった後の画面（正答数表示画面)
-    const finishQuiz = () => {
-      titleElement.textContent = `あなたの正答数は${correctAnswersNum}です`;
-      genreElement.textContent = "";
-      difficultyElement.textContent = "";
-      questionElement.textContent = "再チャレンジしたい場合は下をクリック";
-
-      const restartButton = document.createElement("button");
-      restartButton.textContent = "ホームに戻る";
-      answersContainer.appendChild(restartButton);
-      restartButton.addEventListener("click", () => {
-        location.reload();
-      });
-    };
-
     // 回答数によって処理を切り分ける
     const setNextQuiz = () => {
       while (answersContainer.firstChild) {
@@ -100,84 +85,18 @@
     setNextQuiz();
   };
 
-  //   // クイズの問題数によって処理を切り分けていく
-  //   const answersContainer = document.getElementById("answers");
-  //   const setNextQuiz = (quizInstance, index) => {
-  //     while (answersContainer.firstChild) {
-  //       answersContainer.removeChild(answersContainer.firstChild);
-  //     }
+  // 10問全てのクイズ解答が終わった後の画面（正答数表示画面)
+  const finishQuiz = () => {
+    titleElement.textContent = `あなたの正答数は${correctAnswersNum}です`;
+    genreElement.textContent = "";
+    difficultyElement.textContent = "";
+    questionElement.textContent = "再チャレンジしたい場合は下をクリック";
 
-  //     if (index <= quizInstance.getNumOfQuiz()) {
-  //       makeQuiz(quizInstance, index);
-  //     } else {
-  //       finishQuiz(quizInstance);
-  //     }
-  //   };
-
-  //   // 次の問題
-  //   const genreElement = document.getElementById("genre");
-  //   const difficultyElement = document.getElementById("difficulty");
-
-  //   const makeQuiz = (quizInstance, index) => {
-  //     titleElement.innerHTML = `問題 ${index}`;
-  //     genreElement.innerHTML = `【ジャンル】 ${quizInstance.getQuizCategory(
-  //       index
-  //     )}`;
-  //     difficultyElement.innerHTML = `【難易度】 ${quizInstance.getQuizDifficulty(
-  //       index
-  //     )}`;
-  //     questionElement.innerHTML = `【クイズ】${quizInstance.getQuizQuestion(
-  //       index
-  //     )}`;
-
-  //     const answers = buildAnswers(quizInstance, index);
-
-  //     answers.forEach((answer) => {
-  //       const answerElement = document.createElement("li");
-  //       answersContainer.appendChild(answerElement);
-
-  //       const buttonElement = document.createElement("button");
-  //       buttonElement.innerHTML = answer;
-  //       answerElement.appendChild(buttonElement);
-
-  //       buttonElement.addEventListener("click", () => {
-  //         quizInstance.countCorrectAnswersNum(index, answer);
-  //         index++;
-  //         setNextQuiz(quizInstance, index);
-  //       });
-  //     });
-  //   };
-
-  //   // 解答選択肢（ボタン）を表示
-  //   const buildAnswers = (quizInstance, index) => {
-  //     const answers = [
-  //       quizInstance.getCorrectAnswer(index),
-  //       // 疑問
-  //       ...quizInstance.getIncorrectAnswers(index),
-  //     ];
-  //     return shuffleArray(answers);
-  //   };
-
-  //   const shuffleArray = ([...array]) => {
-  //     for (let i = array.length - 1; i >= 0; i--) {
-  //       const j = Math.floor(Math.random() * (i + 1));
-  //       [array[i], array[j]] = [array[j], array[i]];
-  //     }
-  //     return array;
-  //   };
-
-  //   // 10問全てのクイズ解答が終わった後の画面（正答数表示画面)
-  //   const finishQuiz = (quizInstance) => {
-  //     titleElement.textContent = `あなたの正答数は${quizInstance.getCorrectAnswersNum()}です`;
-  //     genreElement.textContent = "";
-  //     difficultyElement.textContent = "";
-  //     questionElement.textContent = "再チャレンジしたい場合は下をクリック";
-
-  //     const restartButton = document.createElement("button");
-  //     restartButton.textContent = "ホームに戻る";
-  //     answersContainer.appendChild(restartButton);
-  //     restartButton.addEventListener("click", () => {
-  //       location.reload();
-  //     });
-  //   };
+    const restartButton = document.createElement("button");
+    restartButton.textContent = "ホームに戻る";
+    answersContainer.appendChild(restartButton);
+    restartButton.addEventListener("click", () => {
+      location.reload();
+    });
+  };
 }

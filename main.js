89329f3c2869d73effc327e361095b0b31dbfe49
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-04 14:40:10 +0900

    スタートボタンを押してAPIの情報を取得

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-{
-  // const API_URL = "https://opentdb.com/api.php?amount=10&type=multiple";
-}
+const startButton = document.getElementById("start-button");
+startButton.addEventListener("click", () => {
+  fetchQuizdata();
+});
+
+const API_URL = "https://opentdb.com/api.php?amount=10&type=multiple";
+
+const fetchQuizdata = async () => {
+  const response = await fetch(API_URL);
+  const quizData = await response.json();
+  const arrayQuizData = quizData.results;
+};
+
+/**
+ * ①スタートボタンにイベントをつける
+ * ②APIのデータを取得する
+ * ③①と②をつなぐ
+ 
+ * 
+ * スタートボタンを押してゲームがスタートする。
+
+読み込んでいる間、title部分に「取得中」、question部分に「少々お待ちください」と表示される。
+ */

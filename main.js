e43a13b572e9de7b34d195a2cfd5c57dbe990c6b
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-04 17:54:48 +0900

    arrayQuizDataの定義方法変更

--- main.js
+++ main.js
@@ -13,36 +13,30 @@ const fetchQuizdata = async () => {
   questionElement.textContent = "しばらくお待ちください";
   const response = await fetch(API_URL);
   const quizData = await response.json();
-  const arrayQuizData = quizData.results;
-
-  const genreElement = document.getElementById("genre");
-  const difficultyElement = document.getElementById("difficulty");
-
-  const eachQuiz = arrayQuizData[0];
-  let questionCount = 1;
-  titleElement.textContent = `問題${questionCount}`;
-  genreElement.textContent = `カテゴリ：${eachQuiz.category}`;
-  difficultyElement.textContent = `難易度：${eachQuiz.difficulty}`;
-  questionElement.textContent = `問題：${eachQuiz.question}`;
-
-  // スタートボタンを消す
-  startButton.remove();
-  const answerContainer = document.getElementById("answers");
-  const answerElement = document.createElement("li");
-  answerContainer.appendChild(answerElement);
-
-  const buttonElement = document.createElement("button");
-  const correctAnswer = eachQuiz.correct_answer;
-  buttonElement.textContent = correctAnswer;
-  answerElement.appendChild(buttonElement);
 };
 
-/**
- * ①解答を表示するためのHTML要素を作る
- * ②correct_answerの情報を取り出す
- * ③incorrect_answerの情報を取り出す
- * ④HTML要素となげる
- * ⑤④をHTML要素とつなげる
- 
-回答選択肢が表示される。（正解、不正解合わせて4つ）
- */
+function arrayQuizData() {
+  return quizData.results;
+}
+console.log(arrayQuizData);
+
+const genreElement = document.getElementById("genre");
+const difficultyElement = document.getElementById("difficulty");
+
+const eachQuiz = arrayQuizData[0];
+let questionCount = 1;
+titleElement.textContent = `問題${questionCount}`;
+genreElement.textContent = `カテゴリ：${eachQuiz.category}`;
+difficultyElement.textContent = `難易度：${eachQuiz.difficulty}`;
+questionElement.textContent = `問題：${eachQuiz.question}`;
+
+// スタートボタンを消す
+startButton.remove();
+const answerContainer = document.getElementById("answers");
+const answerElement = document.createElement("li");
+answerContainer.appendChild(answerElement);
+
+const buttonElement = document.createElement("button");
+const correctAnswer = eachQuiz.correct_answer;
+buttonElement.textContent = correctAnswer;
+answerElement.appendChild(buttonElement);
